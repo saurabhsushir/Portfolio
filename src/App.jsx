335f6a6a3c4747245fe738206bf66453641cb058
Saurabh Sushir <sushirss1@gmail.com>
2024-12-05 22:46:44 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Technologies from './components/Technologies'
 import Experience from './components/Experience'
 
 
-import Projects from './components/projects'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 const App = () => {
